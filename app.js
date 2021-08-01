5ac0c992b5275f3ba9ff4835a2cdd4b8bcc5f7b7
abeer alzahrani <abooor@outlook.com>
2021-08-01 20:50:56 +0300

    Really Final version

--- app.js
+++ app.js
@@ -228,9 +228,12 @@ app.post("/login", function(req, res){
 });
 
 
+const port = process.env.PORT;
+if(port==numm || port==""){
+  port=3000;
+}
 
 
-
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
+app.listen(port, function(){
+  console.log("Server started Successfully");
 });
